36caa8153fcca032c2ab3af4c582d844fbd58765
Your Name <you@example.com>
2024-08-28 13:38:18 +0900

    modify

--- src/django/smart_datalake.tmp/datalake/smartflow/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from rest_framework import routers
-from .views import *
-
-urlpatterns = [
-    # path('', include(router.urls)),
-    path('get_table_info_by_interval/', GetTableInfoByIntervalView.as_view(), name='get_table_info_by_interval'),
-]
