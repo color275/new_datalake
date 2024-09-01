6daa2cffdd0c2d9687eef2e752e7c63c14b263c2
Your Name <you@example.com>
2024-09-01 18:46:53 +0900

    modify

--- local/dbt/myfirstdbt/macros/pre_logging.sql
+++ local/dbt/myfirstdbt/macros/pre_logging.sql
@@ -1,4 +1,17 @@
 {% macro insert_trans_history() %}
-INSERT INTO src.trans_history (modelname, status, rowcount, startdate, enddate)
-VALUES ('{{ this }}', 'active', 0, now(), now());
+
+{% set start = to_kst_str(var('data_interval_start', 'today_00:00')) %}
+{% set end = to_kst_str(var('data_interval_end', 'tomorrow_00:00')) %}
+
+INSERT INTO src.trans_history (modelname, status, rowcount, startdate, enddate, data_interval_start, data_interval_end)
+VALUES (
+    '{{ this }}',
+    'active',
+    -1,
+    now(),
+    now(),
+    {{ start }}, 
+    {{ end }}
+);
+
 {% endmacro %}
--- local/dbt/myfirstdbt/models/mart/f_orders.sql
+++ local/dbt/myfirstdbt/models/mart/f_orders.sql
@@ -1,6 +1,6 @@
 {{
   config(
-    tags="daily"
+    tags="hourly"
   )
 }}
 
