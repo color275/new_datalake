233b376208ce89daca41a3c0b4f35fb108b4029c
Your Name <you@example.com>
2024-09-03 22:42:13 +0900

    modify

--- src/gen_data/gen.py
+++ src/gen_data/gen.py
@@ -21,24 +21,18 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 # 최대 order_id 조회
-
-
 def get_max_order_id():
     cur.execute("SELECT COALESCE(MAX(order_id), 0) FROM src.orders")
     max_order_id = cur.fetchone()[0]
     return max_order_id
 
 # 기존 order_dt, customer_id, product_id를 가져오는 함수
-
-
 def get_existing_order_data(order_id):
     cur.execute(
         "SELECT order_dt, customer_id, product_id FROM src.orders WHERE order_id = %s", (order_id,))
     return cur.fetchone()
 
 # 초당 삽입할 데이터 건수 설정
-
-
 def insert_bulk_data(batch_size):
     order_id_start = get_max_order_id() + 1  # 최대 order_id의 다음 번호부터 시작
 
@@ -49,12 +43,13 @@ def insert_bulk_data(batch_size):
     while True:
         start_time = time.time()
         insert_query = """
-            INSERT INTO src.orders (order_id, promo_id, order_cnt, order_price, order_dt, last_update_time, customer_id, product_id)
+            INSERT INTO src.orders (order_id, str_order_id, promo_id, order_cnt, order_price, order_dt, last_update_time, customer_id, product_id)
             VALUES %s
         """
         values = []
         for i in range(batch_size):  # batch_size 만큼 삽입
             order_id = order_id_start + i
+            str_order_id = f"abc{str(order_id).zfill(12)}"  # 'abc' + order_id 형식으로 만듦
             promo_id = f"PROMO{random.randint(1, 100):03d}"
             order_cnt = random.randint(1, 10)
             order_price = random.randint(1000, 100000)
@@ -65,10 +60,9 @@ def insert_bulk_data(batch_size):
 
             action = random.choices(
                 ["insert", "update", "delete"], weights=[50, 50, 0], k=1)[0]
-                # ["insert", "update", "delete"], weights=[80, 18, 2], k=1)[0]
 
             if action == "insert":
-                values.append((order_id, promo_id, order_cnt, order_price,
+                values.append((order_id, str_order_id, promo_id, order_cnt, order_price,
                               order_dt, last_update_time, customer_id, product_id))
                 insert_count += 1
             elif action == "update":
@@ -117,8 +111,6 @@ if __name__ == "__main__":
         conn.close()
 
 
-
-
 # import psycopg2
 # import time
 # from datetime import datetime
@@ -144,7 +136,7 @@ if __name__ == "__main__":
 # # 초당 삽입할 데이터 건수 설정
 # def insert_bulk_data(batch_size):
 #     order_id_start = get_max_order_id() + 1  # 최대 order_id의 다음 번호부터 시작
-    
+
 #     insert_count = 0
 #     update_count = 0
 #     delete_count = 0
@@ -168,7 +160,7 @@ if __name__ == "__main__":
 
 #             # 가중치 기반으로 작업 선택 (80% 삽입, 18% 업데이트, 2% 삭제)
 #             action = random.choices(["insert", "update", "delete"], weights=[80, 18, 2], k=1)[0]
-            
+
 #             if action == "insert":
 #                 values.append((order_id, promo_id, order_cnt, order_price, order_dt, last_update_time, customer_id, product_id))
 #                 insert_count += 1
@@ -187,7 +179,7 @@ if __name__ == "__main__":
 
 #         if values:
 #             psycopg2.extras.execute_values(cur, insert_query, values)
-        
+
 #         conn.commit()
 
 #         order_id_start += batch_size  # 다음 벌크 삽입을 위해 order_id 업데이트
